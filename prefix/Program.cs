cc2c26257332197db34527b270786adac8f70f9b
HunyadiPatrik <hunyadipatrik12@gmail.com>
2024-10-02 09:54:13 +0200

    haág

---
+++ prefix/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prefix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10,
+                y = 20;
+
+            Console.WriteLine($"Az x eredeti értéke: {x}");
+            Console.WriteLine($"Az x értée '++x' iratással: {++x}");
+            Console.WriteLine($"Az x értéke kiiratás után: {x}");
+            Console.WriteLine($"\nAz y eredeti értéke: {y}");
+            Console.WriteLine($"Az y értéke 'y++' iratással: {y++}");
+            Console.WriteLine($"Az y értéke kiíratás után: {y}");
+
+            Console.ReadKey();
+        }
+    }
+}
